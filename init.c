20b12ae552b87773b615d02725bb24cf7ba82163
stanislavmg <stanislav.as1998@gmail.com>
2024-06-17 18:38:14 +0400

    FIX: Rewrite initialization function

--- init.c
+++ init.c
@@ -1,147 +1,128 @@
 #include "philo.h"
 
-static int	init_stats(t_stats *stats, char **argv)
+static t_stats	*init_stats(char **argv)
 {
+	t_stats 		*stats;
+
+	stats = (t_stats *)malloc(sizeof(t_stats));
+	if (!stats)
+		return (NULL);
 	stats->philo_num = ft_atoi(argv[1]);
 	stats->ttd = ft_atoi(argv[2]);
 	stats->tte = ft_atoi(argv[3]);
 	stats->tts = ft_atoi(argv[4]);
-	stats->eat_limit = 0;
-	printf("ttd = %ld\ntte = %ld\ntts = %ld\n", stats->ttd, stats->tte, stats->tts);
-	return (0);
+	if (argv[5])
+		stats->eat_limit = ft_atoi(argv[5]);
+	else
+		stats->eat_limit = UNDEFINED;
+	// if (check_stats(stats))
+	// {
+	// 	free(stats);
+	// 	return (NULL);
+	// }
+	return (stats);
 }
 
-t_philo	*init_philo(char **argv)
+static pthread_mutex_t	*forks_init(int num)
 {
-	int	i;
-	int	num;
-	t_philo	*philo;
-	t_stats *st;
 	pthread_mutex_t *forks;
+	int				i;
 
-	i = -1;
-	num = ft_atoi(argv[1]);
-	philo = (t_philo *)malloc(sizeof(t_philo) * num);
+	i = 0;
 	forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * num);
-	st = (t_stats *)malloc(sizeof(t_stats));
-	if (!philo || !forks || !st)
+	if (!forks)
 		return (NULL);
-	init_stats(st, argv);
 	while (i < num)
 	{
-		pthread_mutex_init(forks + i, NULL);
+		if(pthread_mutex_init(forks + i, NULL))
+		{
+			free(forks);
+			return (NULL);
+		}
+		i++;
+	}
+	return (forks);
+}
+
+static t_philo	*init_philo(t_stats *st, pthread_mutex_t *forks)
+{
+	int	i;
+	t_philo *philo;
+
+	i = 0;
+	philo = (t_philo *)malloc(sizeof(t_philo) * st->philo_num);
+	if (!philo)
+		return (NULL);
+	while (i < st->philo_num)
+	{
 		philo[i].eat_count = 0;
 		philo[i].index = i + 1;
 		philo[i].status = 0;
 		philo[i].stats = st;
-		if (i + 1 == num)
+		if ((i + 1) % 2 == 0)
 		{
 			philo[i].left = &forks[i];
-			philo[i].right = &forks[0];
-			break ;
+			philo[i].right = &forks[i + 1];
+		}
+		else if (i + 1 == st->philo_num)
+		{
+			philo[i].left = &forks[0];
+			philo[i].right = &forks[i];
 		}
 		else
+		{
 			philo[i].left = &forks[i + 1];
-		philo[i].right = &forks[i];
+			philo[i].right = &forks[i];
+		}
 		i++;
 	}
-	return (philo);
+	return (philo);	
 }
 
-void create_threads(t_philo *philo)
+int	init(char **argv, t_philo **philo, pthread_mutex_t *forks)
 {
-	int	i;
-	void *status;
-	pthread_t	*th;
+	t_stats 		*st;
 
-	i = -1;
-
-	th = (pthread_t *)malloc(sizeof(pthread_t) * philo->stats->philo_num);
-	if (!th)
-		return ;
-	while (++i < philo->stats->philo_num)
-	{
-		if(pthread_create(th + i, NULL, start_routine, philo + i))
-			return ;
-	}
-	i = -1;
-	while (++i < philo->stats->philo_num)
-	{
-		pthread_join(th[i], &status);
-	}
-}
-
-void	*start_routine(void *arg)
-{
-	t_philo			*philo;
-	
-	philo = (t_philo *)arg;
-	while (1)
-	{
-		if (try_eat(philo))
-			break ;
-		print_action(SLEEP, philo->index, philo->stats);
-		print_action(THINK, philo->index, philo->stats);
-	}
-	return (NULL);
-}
-
-int	try_eat(t_philo *philo)
-{
-	struct timeval	end;
-	struct timeval	start;
-
-	gettimeofday(&start, NULL);
-	pthread_mutex_lock(philo->left);
-	gettimeofday(&end, NULL);
-	if (cmp_time(start, end, philo->stats->ttd))
-	{
-		pthread_mutex_unlock(philo->left);
-		print_action(DEAD, philo->index, philo->stats);
-		return (1);
-	}
-	print_action(FORK, philo->index, philo->stats);
-	pthread_mutex_lock(philo->right);
-	gettimeofday(&end, NULL);
-	if (cmp_time(start, end, philo->stats->ttd))
-	{
-		pthread_mutex_unlock(philo->right);
-		pthread_mutex_unlock(philo->left);
-		print_action(DEAD, philo->index, philo->stats);
-		return (1);
-	}
-	print_action(FORK, philo->index, philo->stats);
-	print_action(EATING, philo->index, philo->stats);
-	pthread_mutex_unlock(philo->right);
-	pthread_mutex_unlock(philo->left);
+	st = NULL;
+	st = init_stats(argv);
+	if (!st)
+		return (ERR_STATS);
+	forks = forks_init(st->philo_num);
+	if (!forks)
+		return (ERR_MUTEX);
+	*philo = init_philo(st, forks);
+	if (!philo)
+		return (ERR_MALLOC);
 	return (0);
 }
 
-int cmp_time(struct timeval start, struct timeval end, suseconds_t limit)
+int create_threads(t_philo *philo, pthread_mutex_t *forks)
 {
-	printf("result = %ld limit = %ld\n", ((end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000), limit);
-	return (limit < ((end.tv_sec - start.tv_sec) * 1000 + (end.tv_usec - start.tv_usec) / 1000));
+	int			i;
+	void 		*status;
+	pthread_t	*th;
+
+	i = 0;
+	th = (pthread_t *)malloc(sizeof(pthread_t) * philo->stats->philo_num + 1);
+	if (!th)
+		return (ERR_MALLOC);
+	if(pthread_create(th + i, NULL, monitoring, philo + i))
+		return (ERR_THREAD);
+	while (++i < philo->stats->philo_num)
+	{
+		if(pthread_create(th + i, NULL, start_routine, philo + i))
+			return (ERR_THREAD);
+		if (pthread_detach(th[i]))
+			return (ERR_THREAD);
+	}
+	pthread_join(th[0], &status);
+	if (!status)
+		exit(1); //TODO
+	free_philo(philo, forks, th);
+	return (0);
 }
 
-void	print_action(int status, int index, const t_stats *stats)
-{
-	struct timeval t;
-	
-	gettimeofday(&t, NULL);
-	if (DEAD == status)
-		printf("%ld %d ++++++++++++++++DIE++++++++++++++++\n", IN_MILISEC(t.tv_usec), index);
-	else if (EATING == status)
-	{
-		printf("%ld %d is eating\n", IN_MILISEC(t.tv_usec), index);
-		usleep(IN_MICROSEC(stats->tte));
-	}
-	else if (SLEEP == status)
-	{
-		printf("%ld %d is sleep\n", IN_MILISEC(t.tv_usec), index);
-		usleep(IN_MICROSEC(stats->tts));
-	}
-	else if (FORK == status)
-		printf("%ld %d has taken a fork\n", IN_MILISEC(t.tv_usec), index);
-	else if (THINK == status)
-		printf("%ld %d is thinking\n", IN_MILISEC(t.tv_usec), index);
-}
+// void	stop_working(t_philo *philo, pthread_mutex_t *forks, pthread_t *th)
+// {
+// 	free_philo(philo, forks, th);
+// }
